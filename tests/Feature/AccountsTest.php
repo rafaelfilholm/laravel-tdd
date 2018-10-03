6d284a01d94a78be963fd05b9739ecb1b0f70781
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-10-02 23:20:41 -0300

    test(accounts): Adicionado teste de criação de contas

---
+++ tests/Feature/AccountsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class AccountsTest extends TestCase
+{
+	use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testApiList()
+    {
+    	/**
+    	 * Create 20 accounts
+    	 */
+    	$data = factory(\App\Account::class, 20)->create();
+        
+        /**
+         * Get response of endpoint /api/accounts
+         */
+        $response = $this->get('/api/accounts');
+
+        /**
+         * Checks if HTTP status code of request is 200
+         * and if has $data of accounts
+         */
+        $response->assertStatus(200)
+        	->assertJson(['data' => $data->toArray()]);
+    }
+}
